d8c25dec594cfc85281225004c8d0915bc29e631
kneeraj825 <kneeraj825@gmail.com>
2020-03-04 10:35:11 +0500

    Added:First name validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo __________Welcome to User_Registration_problem____________
+
+read -p "enter first the word" Word
+firstName="^[A-Z][a-z]{2,}"
+if [[ $Word =~ $firstName  ]]
+then
+echo "its a valid name"
+else
+echo "it is not an valid name"
+fi
