6137d42d918f19289ec46dcf502a9ef7b6eaa951
kneeraj825 <kneeraj825@gmail.com>
2020-03-04 10:30:35 +0500

    Added:Welcome to user registration problem

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,10 +1,3 @@
 #!/bin/bash -x
 
-read -p "enter first the word" Word
-firstName="^[A-Z][a-z]{2,}"
-if [[ $Word =~ $firstName  ]]
-then
-echo "its a valid name"
-else
-echo "it is not an valid name"
-fi
+echo __________Welcome to User_Registration_problem____________
