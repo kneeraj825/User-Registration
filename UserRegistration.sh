d23df59737d10df2a94ef8037ba2bca37f34325f
kneeraj825 <kneeraj825@gmail.com>
2020-03-04 12:03:44 +0500

    Added:Checking last name validation stating with capital letter and has minimum three characters

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -x
 
-echo __________Welcome to User_Registration_problem____________
+#checking the first and last name starting with capital letter and has minimum three characters
+read -p "enter the first name" firstName
+read -p "enter the last name" lastName
 
-read -p "enter first the word" Word
-firstName="^[A-Z][a-z]{2,}"
-if [[ $Word =~ $firstName  ]]
-then
-	echo "its a valid name"
-else
-	echo "it is not an valid name"
-fi
+#checking the validation
+function validation()
+{
+	word=$1
+	name=^[A-Z][a-z]{2,10}$
+	if [[ $word =~ $name  ]]
+	then
+		echo "its a valid name"
+	else
+		echo "it is not an valid name"
+	fi
+}
+firstResult=$(validation $firstName)
+secondResult=$(validation $lastName)
+
